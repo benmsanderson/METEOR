b482256ebfd41d62d7260c119e8ae2f90c408f0d
Ben Sanderson <ben.m.sanderson@gmail.com>
2022-05-04 16:54:01 +0200

    Update prpatt.py

--- prpatt.py
+++ prpatt.py
@@ -20,7 +20,7 @@ def model(pars, x):
     aout=np.empty([nt, nm])
     for i in np.arange(0,nm):
         for j in np.arange(0,nm):
-            print(i,j)
+            print(i,j,nm)
             aout[:,i]=aout[:,i]+expotas(x,vals['s'+str(i)+str(j)],vals['t'+str(i)])
     return aout
 

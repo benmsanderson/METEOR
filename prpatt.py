a1744a0c732b8d5075b5a8c113a8fdc69ef4e40f
benmsanderson <ben.m.sanderson@gmail.com>
2022-05-05 13:12:35 +0000

    colab

--- prpatt.py
+++ prpatt.py
@@ -47,30 +47,57 @@ def makeparams(t0):
 
 def svds(X,nm):
     solver = Eof(X,center=False,weights=wgt(X))
-    v=solver.eofsAsCovariance(neofs=nm)
-    u=solver.pcs(npcs=nm,pcscaling=1)
-    s=solver.eigenvalues(neigs=nm)
-    return u,s,v
+    eofout= {}
+
+    eofout['v']=solver.eofsAsCovariance(neofs=nm)
+    eofout['u']=solver.pcs(npcs=nm,pcscaling=1)
+    eofout['s']=solver.eigenvalues(neigs=nm)
+    return eofout
 
 
 def get_timescales(X,t0):
     nm=len(t0)
     nt=X.shape[0]
-    (u,s,v)=svds(X,nm)
+    eofout=svds(X,nm)
     x_array=np.arange(1,nt+1)
 
     fit_params=makeparams(t0)
 
-    out = lmfit.minimize(residual, fit_params, args=(x_array,nm,), kws={'data': u})
+    out = lmfit.minimize(residual, fit_params, args=(x_array,nm,), kws={'data': eofout['u']})
     #ts=[out.params['t1'].value,out.params['t2'].value,out.params['t3'].value]
     ts=[]
     for i in np.arange(0,nm):
         ts.append(out.params['t'+str(i)].value)
     us=model(out.params,np.arange(0,nt))
-    return (ts,out,us,[u,s,v])
+    return (ts,out,us,eofout)
+
+def get_patterns(X,out):
+    vals = out.params.valuesdict()
+    tsp=[value for key, value in vals.items() if 't' in key.lower()]
+    nm=len(tsp)
+    nt=X.shape[0]
+    eofout=svds(X,nm)
+    x_array=np.arange(1,nt+1)
+    
+   
+    
+    u0=np.empty([nt, len(tsp)])
+    v0=eofout['v']*0.
+    for i,ts in enumerate(tsp):
+      ccv=[value for key, value in vals.items() if 's0' in key.lower()]
+      for j,coef in enumerate(ccv):
+        tmp=expotas(x_array,coef,ts)
+        u0[:,i]=u0[:,i]+tmp
+        v0[:,i]=v0[:,i]+eofout['v'][:,j]*eofout['s'][j]
+    v1=np.reshape(v0,(len(tsp),X.shape[1],X.shape[2]))
+
+    #Xr=np.reshape(np.dot(u1,v1f),X.shape)         
+    va=xr.DataArray(v1, coords=(np.array(tsp),X.lat,X.lon), dims=('mode','lat','lon'))
+    
+    return va,u0
 
 
-def get_patterns(X,tsp):
+def get_patterns_pinv(X,tsp):
     nt=X.shape[0]
     x_array=np.arange(1,nt+1)
     
@@ -79,9 +106,8 @@ def get_patterns(X,tsp):
         tmp=expotas(x_array,1,ts)
         u0[:,i]=tmp/np.mean(tmp)
 
-    u1=u0
     #u1[:,-1]=u0[:,-1]-np.dot(np.dot(u0[:,-1],u0[:,:-1]),np.linalg.pinv(u0[:,:-1]))
-    v1f=np.dot(np.linalg.pinv(u1),X.values.reshape(nt,-1))
+    v1f=np.dot(np.linalg.pinv(u0),X.values.reshape(nt,-1))
     v1=np.reshape(v1f,(len(tsp),X.shape[1],X.shape[2]))
 
     #Xr=np.reshape(np.dot(u1,v1f),X.shape)         

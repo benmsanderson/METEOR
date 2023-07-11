df4a88ed89c859943908639893351a62e8eaf10c
maritsandstad <marit.sandstad@astro.uio.no>
2023-07-11 13:36:58 +0200

    More package stuff in setup

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ setup(
     keywords=["emulator", "pattern scaling"],
     packages=PACKAGES,
     package_dir=PACKAGE_DIR,
-    # package_data=PACKAGE_DATA,
+    package_data=PACKAGE_DATA,
     include_package_data=True,
     install_requires=REQUIREMENTS,
     extras_require=REQUIREMENTS_EXTRAS,

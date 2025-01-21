4e728021b30dc8ec854b6b56c00f582d762a931a
maritsandstad <marit.sandstad@astro.uio.no>
2025-01-21 16:00:40 +0100

    Syncing initial guesses for aerosol timescales to that of GHG

--- src/meteor/prpatt.py
+++ src/meteor/prpatt.py
@@ -699,7 +699,7 @@ def get_timescales_from_anomaly(residual_anom, fcg_aer, n_modes=2):
     bounds = [(0.1, 10 ** (mode_num + 2)) for mode_num in range(n_modes)]
     opt = minimize(
         rmse_gm,
-        [np.mean(bound) for bound in bounds],
+        [10 ** (mode_num + 1) for mode_num in range(n_modes)],
         args=(gmanom, fcg_aer),
         bounds=bounds,
         method="Powell",

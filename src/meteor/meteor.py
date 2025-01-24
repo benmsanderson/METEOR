b8e75f811a657f747a38328b233ba0b095788c2b
maritsandstad <marit.sandstad@astro.uio.no>
2025-01-24 11:49:33 +0100

    Changing so anomaly timescales can be different for different fields

--- src/meteor/meteor.py
+++ src/meteor/meteor.py
@@ -98,7 +98,7 @@ class MeteorPatternScaling:
         exp_list,
         from_file=True,
         ssp_input=None,
-        anom_timescales=1,
+        anom_timescales=None,
     ):  # pylint: disable=too-many-arguments, too-many-positional-arguments
         """
         Initialise Pattern Scaling object
@@ -127,6 +127,14 @@ class MeteorPatternScaling:
         self.exp_list = exp_list
         self.patternflds = patternflds
         self.pattern_dict = self._make_pattern_dict()
+        if anom_timescales is None or not isinstance(anom_timescales, dict):
+            anom_timescales = {}
+            for fld in patternflds:
+                anom_timescales[fld] = 1
+        else:
+            for fld in patternflds:
+                if fld not in anom_timescales:
+                    anom_timescales[fld] = 1
         self.anom_timescales = anom_timescales
         if "xanom" in "-".join(exp_list):
             self._add_patterns_for_residual_exp(ssp_input)
@@ -217,7 +225,7 @@ class MeteorPatternScaling:
                 self.dacanom[fld][exp_index, :em_len, :, :] - predicted_without_fld
             )
             (out, pattern_full) = prpatt.get_timescales_from_anomaly(
-                residual, forcing_of_residual, n_modes=self.anom_timescales
+                residual, forcing_of_residual, n_modes=self.anom_timescales[fld]
             )
             self.pattern_dict[exp][fld]["pattern_full"] = pattern_full
             self.pattern_dict[exp][fld]["outp"] = out

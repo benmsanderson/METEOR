Fixing scaling
(cherry picked from commit 4f5df801a0ca3efeb083ee797e9745fc9273092f)

src/meteor/geo_data_utils.py
@@ -34,6 +34,18 @@ def get_time_name(ds):
             return time_name
     raise RuntimeError("Couldn't find a time coordinate")
 
+def get_year_series(ds: xr.Dataset) -> np.ndarray:
+    """Extract a DataFrame of just the year columns from a larger DataFrame."""
+    time_name = get_time_name(ds)
+    if time_name == "year":
+        return ds[time_name].values
+    elif time_name == "time":
+        return ds[time_name].dt.year.values
+    elif time_name == "month":
+        return ds[time_name].dt.year.values + (ds[time_name].dt.month.values - 1) / 12.0
+    else:
+        raise ValueError(f"Unexpected time dimension name '{time_name}'")
+
 
 def get_lat_name(ds):
     """
@@ -509,12 +521,8 @@ def extend_temeperature_anomaly_timeseries_for_scaling(
     temperature_input_anomaly : xarray.DataArray
         Temperature anomaly time series used for scaling (relative to base year)
     """
-    years_prediction = annual_temp_prediction_anomaly_gm[
-        get_time_name(annual_temp_prediction_anomaly_gm)
-    ].values
-    years_input = temperature_input_anomaly[
-        get_time_name(temperature_input_anomaly)
-    ].values
+    years_prediction = get_year_series(annual_temp_prediction_anomaly_gm)
+    years_input = get_year_series(temperature_input_anomaly)
     temperature_input_anomaly_extended = np.copy(
         annual_temp_prediction_anomaly_gm.values
     )
@@ -523,11 +531,13 @@ def extend_temeperature_anomaly_timeseries_for_scaling(
             temperature_input_anomaly_extended[i] = temperature_input_anomaly.sel(
                 {get_time_name(temperature_input_anomaly): year}
             ).values
+
     temperature_input_anomaly_extended = xr.DataArray(
         data=temperature_input_anomaly_extended,
         coords={get_time_name(annual_temp_prediction_anomaly_gm): years_prediction},
         dims=annual_temp_prediction_anomaly_gm.dims,
     )
+
     return temperature_input_anomaly_extended
 
 